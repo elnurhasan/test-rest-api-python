7140f481554e467be269e16156bc72ec14748f49
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-07 13:48:29 +0300

    Add method for passed tests

--- user/models.py
+++ user/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from subscribe.models import Subscribe
 
 class User(models.Model):
     fullname = models.CharField(max_length=64, verbose_name='Full name', unique=True)
 
     def __str__(self) -> str:
         return self.fullname
+    
+    def subscribe_to(self, blog) -> bool:
+        """Subscribes the user to other users' blogs"""
+        Subscribe.objects.create(
+            user = self,
+            blog = blog
+        )
+
+    @property
+    def subscribed_blogs(self):
+        return self.subscribes.all()
