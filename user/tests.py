7506ba5940e22f1eccb9eb28624de15da06ef081
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-07 13:33:38 +0300

    Add test subscribe to blog

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from user.models import User
+from blog.models import Blog
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        self.first_user = User.objects.create(
+            fullname='First user'
+        )
+        self.second_user = User.objects.create(
+            fullname='Second user'
+        )
+        self.third_user = User.objects.create(
+            fullname='Third user'
+        )
+
+    def test_subscribe_to_blog(self):
+        blog = self.second_user.blog
+        self.first_user.subscribe_to(blog)
+        self.assertEqual(len(self.first_user.subscribed_blogs), 1)
+
+        blog = self.third_user.blog
+        self.first_user.subscribe_to(blog)
+        self.assertEqual(len(self.first_user.subscribed_blogs), 2)
