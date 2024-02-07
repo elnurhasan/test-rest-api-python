6ab1c201f0b72d0ed6c27273705b8b9801babe43
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-07 12:48:20 +0300

    Add blog & post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='Author')
+
+    def __str__(self) -> str:
+        return self.author.fullname
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128, verbose_name='Title')
+    content = models.CharField(max_length=140, null=True, blank=True, verbose_name='Content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
+
+    def __str__(self) -> str:
+        return self.title
+
