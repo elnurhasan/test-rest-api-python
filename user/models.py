ecaa8037236c6edcc101218b20bd32c4533d8411
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-07 12:38:11 +0300

    Add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    fullname = models.CharField(max_length=64, verbose_name='Full name', unique=True)
+
+    def __str__(self) -> str:
+        return self.fullname
