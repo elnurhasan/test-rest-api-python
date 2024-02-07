2484c6e52e27638165d0add81ceffd4ce102cc46
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-07 13:18:29 +0300

    Add check signal test for blog

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from user.models import User
+from blog.models import Blog
 
-# Create your tests here.
+
+class BlogTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_blog_when_user_created(self):
+        before_blogs = Blog.objects.count()
+        user = User.objects.create(
+            fullname='Test user'
+        )
+        after_blogs = Blog.objects.count()
+        self.assertNotEqual(before_blogs, after_blogs)
+
+    def test_delete_blog_when_user_deleted(self):
+        before_blogs = Blog.objects.count()
+        user = User.objects.create(
+            fullname='Test user'
+        )
+        user.delete()
+        after_blogs = Blog.objects.count()
+
+        self.assertEqual(before_blogs, after_blogs)
