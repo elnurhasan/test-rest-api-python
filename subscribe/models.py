ea9895c0741f4989cf32d40ad794912d65206d8b
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-07 13:43:04 +0300

    Fix circular Exception

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from user.models import User
-from blog.models import Blog
 
 class Subscribe(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscribes')
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='subscribes')
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='subscribes')
+    blog = models.ForeignKey('blog.Blog', on_delete=models.CASCADE, related_name='subscribes')
