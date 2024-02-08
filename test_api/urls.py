289840a0a856bb0dd5d3610def050376cb011cd5
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-08 17:18:14 +0300

    Add Root api route for first page

--- test_api/urls.py
+++ test_api/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api import views
 
 urlpatterns = [
+    path('', views.api_root),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
