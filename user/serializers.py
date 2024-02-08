6666c9c0c640834c4285cc556b8094de5c5b5375
Elnur Hasanov <hasanov.elnur@gmail.com>
2024-02-08 13:36:58 +0300

    Add news_list field for user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-from user.models import User
+from user.models import User, News
 
 
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     fullname = serializers.CharField(required=True, allow_blank=False, max_length=64)
+    news_list = serializers.PrimaryKeyRelatedField(many=True, queryset=News.objects.all())
     
 
     def create(self, validated_data):
